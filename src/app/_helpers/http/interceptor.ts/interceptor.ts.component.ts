29d82ac5203a8499bc0633622f9ecf3f0263c6fe
Gilmar <grafos@outlook.com.br>
2024-06-10 17:48:07 -0300

    continuando

--- src/app/_helpers/http/interceptor.ts/interceptor.ts.component.ts
+++ src/app/_helpers/http/interceptor.ts/interceptor.ts.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-interceptor.ts',
   templateUrl: './interceptor.ts.component.html',
   styleUrl: './interceptor.ts.component.css'
 })
-export class InterceptorTsComponent {
+export class InterceptorTsComponent implements HttpInterceptor{
 
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      withCredentials: true,
+    });
+
+    return next.handle(req);
+  }
 }
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+];
